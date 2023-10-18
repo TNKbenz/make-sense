beb15db01ae7efbe42c791ed2254d437139ac306
Thanasak-s63 <s6301012620049@email.kmutnb.ac.th>
2023-10-19 02:17:41 +0700

    predict image

--- src/views/Predict/ImageDropzone.tsx
+++ src/views/Predict/ImageDropzone.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import axios from 'axios';
-import './Predict.css';
-import { PieChart, Pie } from 'recharts';
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import "./Predict.css";
+import { PieChart, Pie } from "recharts";
 
 interface ImageDropzoneProps {
   onUploadSuccess: (predictions: Predictions) => void;
@@ -12,27 +12,28 @@ type Predictions = {
   [key: string]: number;
 };
 
-const data = {
-  "golden retriever": 0.8504292964935303,
-  "Labrador retriever": 0.08213566243648529,
-  "cocker spaniel": 0.01636691391468048,
-  "kuvasz": 0.0071950675919651985,
-  "Tibetan terrier": 0.005311739630997181
-};
+// const data = {
+//   "golden retriever": 0.8504292964935303,
+//   "Labrador retriever": 0.08213566243648529,
+//   "cocker spaniel": 0.01636691391468048,
+//   kuvasz: 0.0071950675919651985,
+//   "Tibetan terrier": 0.005311739630997181,
+// };
 
-const colors = ['#8884d8', '#82ca9d', '#ffc658', '#ff7f0e', '#d1300e'];
+const colors = ["#8884d8", "#82ca9d", "#ffc658", "#ff7f0e", "#d1300e"];
 
 const ImageDropzone: React.FC<ImageDropzoneProps> = ({ onUploadSuccess }) => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [selectedFileUrl, setSelectedFileUrl] = useState<string | null>(null);
   const [predictions, setPredictions] = useState<Predictions | null>(null);
+  const [predict_key, setPredict_key] = useState<string | null>(null);
 
   const onDrop = useCallback(async (acceptedFiles: File[]) => {
     if (acceptedFiles.length > 0) {
       const file = acceptedFiles[0];
 
       // Resize the image to 300x300 pixels
-      const resizedImage = await resizeImage(file, 400,400);
+      const resizedImage = await resizeImage(file, 400, 400);
       setSelectedFile(resizedImage);
       setSelectedFileUrl(URL.createObjectURL(resizedImage));
     }
@@ -40,38 +41,52 @@ const ImageDropzone: React.FC<ImageDropzoneProps> = ({ onUploadSuccess }) => {
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    // accept: 'image/*',
+    accept: {
+      "image/*": [".jpeg", ".png"],
+    },
   });
 
   const handleUpload = () => {
     if (selectedFile) {
       const formData = new FormData();
-      formData.append('file', selectedFile);
-      
-      
+      formData.append("file", selectedFile);
+
       axios
-        .post('http://localhost:3001/upload', formData)
+        .post("http://localhost:8000/uploadfile/", formData)
         .then((response) => {
-          const receivedPredictions: Predictions = response.data.predictions;
+          const receivedPredictions: Predictions = response.data;
           setPredictions(receivedPredictions);
+          onUploadSuccess(receivedPredictions);
+          console.log(receivedPredictions);
+
+          const Predict_Key: string = Object.keys(receivedPredictions).reduce(
+            (mostLikely, key) =>
+              receivedPredictions[key] > receivedPredictions[mostLikely]
+                ? key
+                : mostLikely,
+            Object.keys(receivedPredictions)[0]
+          );
+          setPredict_key(Predict_Key);
           onUploadSuccess(receivedPredictions);
         })
         .catch((error) => {
-          console.error('เกิดข้อผิดพลาดในการอัพโหลดไฟล์', error);
+          console.error("เกิดข้อผิดพลาดในการอัพโหลดไฟล์", error);
         });
-    
-        setPredictions(data)
     }
   };
 
-  const resizeImage = async (file: File, maxWidth: number, maxHeight: number) => {
+  const resizeImage = async (
+    file: File,
+    maxWidth: number,
+    maxHeight: number
+  ) => {
     return new Promise<File>((resolve) => {
       const img = new Image();
       img.src = URL.createObjectURL(file);
 
       img.onload = () => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
 
         if (ctx) {
           const width = img.width;
@@ -90,14 +105,14 @@ const ImageDropzone: React.FC<ImageDropzoneProps> = ({ onUploadSuccess }) => {
           canvas.toBlob((blob) => {
             if (blob) {
               const resizedFile = new File([blob], file.name, {
-                type: 'image/jpeg',
+                type: "image/jpeg",
                 lastModified: Date.now(),
               });
               resolve(resizedFile);
             } else {
               resolve(file);
             }
-          }, 'image/jpeg');
+          }, "image/jpeg");
         } else {
           resolve(file);
         }
@@ -107,42 +122,47 @@ const ImageDropzone: React.FC<ImageDropzoneProps> = ({ onUploadSuccess }) => {
 
   return (
     <div>
-      <div {...getRootProps()} className='image-dropzone'>
+      <div {...getRootProps()} className="image-dropzone">
         <input {...getInputProps()} />
-        <p className='Predict-image-dropzone'>Upload Image</p>
+        <p className="Predict-image-dropzone">Upload Image</p>
       </div>
       {selectedFile && (
         <div>
           <p></p>
-          <img src={selectedFileUrl} alt='Selected' className='Selected-Image' />
+          <img
+            src={selectedFileUrl}
+            alt="Selected"
+            className="Selected-Image"
+          />
           <p>ไฟล์ที่เลือก: {selectedFile.name}</p>
           <p>ประเภทไฟล์ที่เลือก: {selectedFile.type}</p>
-          <button onClick={handleUpload}>Predict</button> 
+          <button onClick={handleUpload}>Predict</button>
         </div>
-      )} 
+      )}
       {predictions && (
-      <div>
-        <h2>คาดว่าเป็น: golden retriever {predictions["golden retriever"] * 100}%</h2>
-        <div className='Chart' >
-          <PieChart width={700} height={700}>
-            <Pie
-              data={Object.keys(data).map((key, index) => ({
-                name: key,
-                value: data[key],
-                fill: colors[index % colors.length],
-              }))}
-              cx="50%"
-              cy="50%"
-              innerRadius={75}
-              outerRadius={150}
-              dataKey='value'
-              label
-            >
-            </Pie>
-          </PieChart>
+        <div>
+          <h2>
+            คาดว่าเป็น: {predict_key} {predictions[predict_key] * 100}%
+          </h2>
+          <div className="Chart">
+            <PieChart width={700} height={700}>
+              <Pie
+                data={Object.keys(predictions).map((key, index) => ({
+                  name: key,
+                  value: predictions[key],
+                  fill: colors[index % colors.length],
+                }))}
+                cx="50%"
+                cy="50%"
+                innerRadius={75}
+                outerRadius={150}
+                dataKey="value"
+                label
+              ></Pie>
+            </PieChart>
+          </div>
         </div>
-      </div>
-    )}
+      )}
     </div>
   );
 };
--- src/views/Train/Components/Tab_Beginners.tsx
+++ src/views/Train/Components/Tab_Beginners.tsx
@@ -1,25 +1,66 @@
-import React, { FC, Fragment ,useState} from "react";
-import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { FC, Fragment, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { LabelsSelector } from "../../../store/selectors/LabelsSelector";
+import { findLast } from "lodash";
 
 const Tab_Beginners: FC<{}> = () => {
-    const navigate = useNavigate();  
-    const [epoch, setEpoch] = useState<string>("");
-    const [learningRate, setLearningRate] = useState<string>("");
-   
+  const navigate = useNavigate();
+  const [epoch, setEpoch] = useState<string>("");
+  const [learningRate, setLearningRate] = useState<string>("");
+
+  // const imageDatas = LabelsSelector.getImagesData();
+  // console.log(imageDatas);
+  // for (const imageData of imageDatas) {
+  //   const labelNames = LabelsSelector.getLabelNames();
+  //   let a = imageData.labelNameIds.map((labelNameId: string) => {
+  //     return findLast(labelNames, { id: labelNameId }).name;
+  //   });
+  //   console.log(a);
+  //   console.log(labelNames);
+  // }
+
+  //send training data to backend
+  const handleSubmit = async () => {
+    console.log("data submitted");
+    // try {
+    //   const data = {
+    //     epoch,
+    //     lr: learningRate,
+    //   };
+    //   const response = await axios.post("http://localhost:8000/train/", data);
+    //   console.log(response.data);
+    //   // Handle any other logic after a successful request, if needed.
+    // } catch (error) {
+    //   // Handle errors, e.g., display an error message or log the error.
+    //   console.error("Error:", error);
+    // }
+  };
+
   return (
     <Fragment>
       <div className="Train">
-        <button onClick={() => navigate('/predict')}>Create Model</button>
-        <button onClick={() => navigate('/predict')}>Select Existing Model</button>
-        
-        </div>
-        <div className="Parameter">
-            <h3>Model tuning</h3>
-            <div>Epoch</div>
-            <input  type="text" value={epoch} onChange={e => setEpoch(e.target.value)} />
-            <div>Learning Rate</div>
-            <input  type="text" value={learningRate} onChange={e => setLearningRate(e.target.value)} />
-        </div>
+        <button onClick={() => navigate("/predict")}>Create Model</button>
+        <button onClick={() => navigate("/predict")}>
+          Select Existing Model
+        </button>
+      </div>
+      <div className="Parameter">
+        <h3>Model tuning</h3>
+        <div>Epoch</div>
+        <input
+          type="text"
+          value={epoch}
+          onChange={(e) => setEpoch(e.target.value)}
+        />
+        <div>Learning Rate</div>
+        <input
+          type="text"
+          value={learningRate}
+          onChange={(e) => setLearningRate(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Train</button>
+      </div>
     </Fragment>
   );
 };
